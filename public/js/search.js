951c3c7677cdb1cccd41c5da7ebf0a9fb00e26db
oyama <oyama@projectcodex.co>
2015-12-27 16:48:14 +0200

    no more errors found in console, next thing to do is to be able to upload a an image file

---
+++ public/js/search.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+        //Products Search
+  $("#search").keyup(function(){
+    var issueTdPullreq = $("#issueTdPullreq").val();
+    $.get("/issues/search/" + issueTdPullreq, function(results){
+      $("#reqIssuesOutPut").html(results);
+      });
+            // conditional statements as a layout manager for a proper search textBox output
+                if (issueTdPullreq === true) {
+        $.get("/issues/search/" + issueTdPullreq, function(results){
+                    $("#reqIssuesOutPut").html(results);
+          });
+
+                } else if(issueTdPullreq === "" || false){
+                    window.location.replace("http://localhost:2010/issues");
+
+                  };
+
+    });
+});
