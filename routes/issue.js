a3896caeabcd4d7a58eeba843b334565fa409eb2
oyama <oyama@projectcodex.co>
2015-12-27 23:24:47 +0200

    modified data pool requestr

--- routes/issue.js
+++ routes/issue.js
@@ -31,17 +31,8 @@ exports.show = function(req, res, next) {
 			connection.query('SELECT issue_id, issue_date, issues.subject, issues.id, category_td.category, issues.issue_description, issues.error_message, issues.Solution FROM issues, category_td WHERE issues.id = category_td.id GROUP BY subject ORDER BY issue_date',[data], function(err,results){
 				if (err)
               		return next("Error Selecting : %s ",err );
-              	connection.query('SELECT id, category from category_td', [data.id], function(err, categoryList){
-				if(err)
-					return next("Error Selecting : %s ", err);
-					var issue = results[0];
-					var categories = categoryList.map(function(category){
-						return {
-							id : category.id,
-							category : category.category,
-							selected : category.id === issue.id
-						}
-					});
+              	+
+              	
 
 					res.render('issues',{
 						results:results,
